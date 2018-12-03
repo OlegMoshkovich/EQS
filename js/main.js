4d335dcf644092f9afdd5c43acbbd6a1a5156903
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-12-03 16:18:44 -0500

    First commit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
 // document.querySelector('#focus').style.background = getRandomColor()
 
-var array = ['Frontend engineering', 'Product Design', 'Computational Design', 'Balance'];
+var array = ['Frontend engineering', 'Product Design',  'Balance.'];
+
+
+
 array.forEach(function (el,index) {
   setTimeout(function () {
     document.querySelector('#focus').innerHTML = el;
