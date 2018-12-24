fbee31a517c1a2c552108e6fd1824fc015390f37
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-12-23 19:22:23 -0500

    added cover property to the navigation portion and the image cycle

--- js/main.js
+++ js/main.js
@@ -1,12 +1,1 @@
-// document.querySelector('#focus').style.background = getRandomColor()
 
-var array = ['Frontend engineering', 'Product Design',  'Balance.'];
-
-
-
-array.forEach(function (el,index) {
-  setTimeout(function () {
-    document.querySelector('#focus').innerHTML = el;
-    console.log(el);
-  }, index * 2000);
-})
