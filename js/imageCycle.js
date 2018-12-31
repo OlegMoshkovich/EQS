4f125ced75697b9de76508f2bdfbe04c3e81e48c
Oleg Moshkovich <oleg.mosh@gmail.com>
2018-12-31 16:37:02 -0500

    styled pop up and added new location to the map

--- js/imageCycle.js
+++ js/imageCycle.js
@@ -1,5 +1,6 @@
 
 const images = ['url(../img/horse_lover.png)','url(../img/fog.png)','url(../img/horses-beauty.png)','url(../img/horses-pack.png)','url(../img/horse-lover-2.png)']
+
 const imageCycle = () => {
   const imageRandom =  Math.floor(Math.random()*images.length);
   document.querySelector('.navigation__nav').style.backgroundImage=images[imageRandom]
--- js/main.js
+++ js/main.js
@@ -1,70 +1,67 @@
-// import imageCycle from 'imageCycle';
-
 const coordinates = [-73.985861,40.7370135]; //starting position
-
 const naviagtionList = document.querySelectorAll('.navigation__item');
-
 naviagtionList.forEach(element => {
   element.addEventListener('click', () => {
-    imageCycle();
     fly();
-  }
-    );
+  });
 });
 
-function fly(){
-  var city = event.srcElement.textContent;
-  console.log(event.srcElement.textContent)
-  if(event.srcElement.textContent === 'New York'){
-    city = 'NewYork';
-  }
-  var location = {
-    NewYork:[-73.985861,40.7370135],
-    Tokyo:[139.78041,35.54843],
-    Zurich:[8.54,47.37861],
-    Paris:[2.320582,48.859489],
-    Berlin:[13.393236,52.504043],
-    London:[-0.1275,51.50722]
-  }
-  console.log(location[city]);
+mapboxgl.accessToken = 'pk.eyJ1Ijoib2xlZ21vc2hrb3ZpY2giLCJhIjoiY2pmeTFidnQzMGUwaDMycTd6aGlseXF6ayJ9._4zzVy5_Q5lPjIiN56SMyQ';
 
-  map.flyTo({
-  center: location[city],
-  zoom: 7
-  });
-
-  }
-
-  mapboxgl.accessToken = 'pk.eyJ1Ijoib2xlZ21vc2hrb3ZpY2giLCJhIjoiY2pmeTFidnQzMGUwaDMycTd6aGlseXF6ayJ9._4zzVy5_Q5lPjIiN56SMyQ';
-
-  const map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/olegmoshkovich/cjpac1bs09b7w2ro70qt24lna',
+  style: 'mapbox://styles/olegmoshkovich/cjpcalvrmb55l2ro7xypgw0s3',
+  // style: 'mapbox://styles/olegmoshkovich/cjpac1bs09b7w2ro70qt24lna',
   center: [coordinates[0],coordinates[1]],
   zoom: 7.4
-  });
+});
+
+
+
+const fly = () => {
+    const city = event.srcElement.textContent;
+    //new york is the edge case bacause of the space between two words
+    if(event.srcElement.textContent === 'New York'){
+      city = 'NewYork';
+    }
+    var location = {
+      NewYork:[-73.985861,40.7370135],
+      Tokyo:[139.78041,35.54843],
+      Zurich:[8.54,47.37861],
+      Paris:[2.320582,48.859489],
+      Berlin:[13.393236,52.504043],
+      London:[-0.1275,51.50722]
+    }
+    map.flyTo({
+    center: location[city],
+    zoom: 7})
+  }
+
+
 
 
   map.on('click', function(e) {
 
-  var features = map.queryRenderedFeatures(e.point, {
-    layers: ['eqs-dataset'] // replace this with the name of the layer
-  });
+    var features = map.queryRenderedFeatures(e.point, {
+      layers: ['eqs-data']
+    });
 
-  if (!features.length) {
-    return;
-  }
+    if (!features.length) {
+      return;
+    }
 
   var feature = features[0];
-
-  // Create a popup, but don't add it to the map yet.
-
 
   var popup = new mapboxgl.Popup({ offset: [0, -15] })
 
     .setLngLat(feature.geometry.coordinates)
-    .setHTML('<div>' + feature.properties.title + '</div><p>' + feature.properties.description + '</p>')
+    .setHTML(
+      '<div class = "popup_title">' + feature.properties.title + '</div>' +
+      '<div class = "popup_desc">' + feature.properties.description + '</div>'+
+      '<div>'  + `<img src=${feature.properties.image} class = "popup_img">` + '</div>'
+    )
     .setLngLat(feature.geometry.coordinates)
     .addTo(map);
 
   });
+// "../img/fog.png"
